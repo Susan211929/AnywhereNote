b5ddfc626a96a6d74fecc316cbb9b28908239629
Susan Thomas <susan.roslyn@gmail.com>
2018-03-13 09:09:09 +0530

    Overflow and navbar

--- app/src/main/java/com/notes/anywherenote/anywherenote/MainActivity.java
+++ app/src/main/java/com/notes/anywherenote/anywherenote/MainActivity.java
@@ -2,14 +2,20 @@ package com.notes.anywherenote.anywherenote;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,CreateOption.class));
             }
         });
+
+        /*Inserting navigation bar*/
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);//Setting overflow menu
+        NavigationView navigationview = (NavigationView) findViewById(R.id.navigationview);
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, toolbar,R.string.drawer_open,R.string.drawer_close);
+        mDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {         //For overflow menu
+        getMenuInflater().inflate(R.menu.overflow_menu, menu);
+        return true;
+    }
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sign_out: {
+                // do your sign-out stuff
+                break;
+            }
+            // case blocks for other MenuItems (if any)
+        }
+        return false;
+    }*/
 }
