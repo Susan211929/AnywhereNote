035550be7789aa4f01c45cddec6311bc3d026dc5
Susan Thomas <susan.roslyn@gmail.com>
2018-04-05 00:58:18 +0530

    Changing of note color and checklist dialog box working
    Note colour-> pop up window working and successfully changing colour of background
    Checklits-> Dialog box for adding new item working

--- app/src/main/java/com/notes/anywherenote/anywherenote/CreateChecklist.java
+++ app/src/main/java/com/notes/anywherenote/anywherenote/CreateChecklist.java
@@ -1,16 +1,43 @@
 package com.notes.anywherenote.anywherenote;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by susan_000 on 04-Mar-18.
  */
 
 public class CreateChecklist extends Activity {
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.checklist);
+
+        ImageView add = (ImageView) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder adb = new AlertDialog.Builder(CreateChecklist.this);
+                View add_chcklst = getLayoutInflater().inflate(R.layout.add_checklist,null);
+                EditText input = (EditText) add_chcklst.findViewById(R.id.txt_edt);
+                Button add_btn = (Button) add_chcklst.findViewById(R.id.btn_add);
+                add_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(CreateChecklist.this, "Yaaaaayyyyy!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                adb.setView(add_chcklst);
+                adb.create().show();
+            }
+        });
     }
 }
--- app/src/main/java/com/notes/anywherenote/anywherenote/CreateText.java
+++ app/src/main/java/com/notes/anywherenote/anywherenote/CreateText.java
@@ -1,12 +1,15 @@
 package com.notes.anywherenote.anywherenote;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.PopupWindow;
 
 /**
  * Created by susan_000 on 04-Mar-18.
@@ -18,11 +21,70 @@ public class CreateText extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text);
 
-        ImageView colour = (ImageView) findViewById(R.id.select_colour);
+        /*To change colour of note*/
+        final EditText input = (EditText) findViewById(R.id.txt_input);
+        final ImageView colour = (ImageView) findViewById(R.id.select_colour);
         colour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(CreateText.this,ColourSelection.class));
+                View sel_col = getLayoutInflater().inflate(R.layout.select_colour,(ViewGroup) findViewById(R.id.popUp));
+                final PopupWindow pw = new PopupWindow(sel_col, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,true);
+                pw.showAtLocation(sel_col, Gravity.CENTER, 0, 0);
+                ImageView blue = (ImageView) sel_col.findViewById(R.id.sel_blue);
+                ImageView green = (ImageView) sel_col.findViewById(R.id.sel_green);
+                ImageView orange = (ImageView) sel_col.findViewById(R.id.sel_orange);
+                ImageView red = (ImageView) sel_col.findViewById(R.id.sel_red);
+                ImageView purple = (ImageView) sel_col.findViewById(R.id.sel_purple);
+                ImageView white = (ImageView) sel_col.findViewById(R.id.sel_white);
+
+                blue.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        colour.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.blue));
+                        input.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.blue));
+                        pw.dismiss();
+                    }
+                });
+                green.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        colour.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.green));
+                        input.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.green));
+                        pw.dismiss();
+                    }
+                });
+                orange.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        colour.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.orange));
+                        input.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.orange));
+                        pw.dismiss();
+                    }
+                });
+                red.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        colour.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.red));
+                        input.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.red));
+                        pw.dismiss();
+                    }
+                });
+                purple.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        colour.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.purple));
+                        input.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.purple));
+                        pw.dismiss();
+                    }
+                });
+                white.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        colour.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.white));
+                        input.setBackgroundColor(ContextCompat.getColor(CreateText.this, R.color.white));
+                        pw.dismiss();
+                    }
+                });
             }
         });
     }
